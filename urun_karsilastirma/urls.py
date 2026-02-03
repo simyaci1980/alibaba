cb70151afc1a8f81b8f358cdc9ef56253acf4b72
ALİ ALTINSOY 111777 <AB111777@adalet.gov.tr>
2026-02-03 12:27:00 +0300

    favicon.ico redirect ekle

--- urun_karsilastirma/urls.py
+++ urun_karsilastirma/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('urunler.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/urunler/favicon.ico', permanent=True)),
 ]
 
 if settings.DEBUG:
