fd57acc2d90b6f58bd6a4b0f3309d236e69070fc
ALİ ALTINSOY 111777 <AB111777@adalet.gov.tr>
2026-04-01 14:34:09 +0300

    Config: allow sqlite path outside repo and ignore sqlite files

--- urun_karsilastirma/settings.py
+++ urun_karsilastirma/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'urun_karsilastirma.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('SQLITE_DB_PATH', default=str(BASE_DIR / 'db.sqlite3')),
     }
 }
 
