cb8d6524219140519d9d539433331cca19bd53ff
ALİ ALTINSOY 111777 <AB111777@adalet.gov.tr>
2026-04-01 14:52:50 +0300

    Deploy: PythonAnywhere WSGI path and settings module

--- urun_karsilastirma/wsgi.py
+++ urun_karsilastirma/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for urun_karsilastirma project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# add your project directory to the sys.path
+project_home = '/home/KolayBulExpres/alibaba'
+if project_home not in sys.path:
+	sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'urun_karsilastirma.settings'
+
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urun_karsilastirma.settings')
 
 application = get_wsgi_application()
